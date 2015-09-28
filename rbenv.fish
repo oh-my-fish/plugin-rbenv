3c043860e66fb4214d4c33d6e8381b535d7706c1
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-09-29 00:28:22 +0100

    Fix #12 - correctly create function rbenv
    - No need to check if status --is interactive?
    - Separate `init` and `rbenv` functions.

--- rbenv.fish
+++ rbenv.fish
@@ -10,27 +10,31 @@ function init --on-event init_rbenv
     echo "Please install 'rbenv' first, or set \$RBENV_ROOT!"; return 1
   end
 
-  if status --is-interactive
-    if rbenv init - | grep --quiet "function"
-      source (rbenv init - | psub)
-    else
-      if not set -q RBENV_ROOT
-        set -x RBENV_ROOT "$HOME/.rbenv"
-      end
+  if command rbenv init - | grep --quiet "function"
+    set -gx __RBENV_SUPPORTS_FISH true
+    source (rbenv init - | psub)
+  else
+    if not set -q RBENV_ROOT
+      set -x RBENV_ROOT "$HOME/.rbenv"
+    end
 
-      set PATH "$RBENV_ROOT/shims" $PATH
+    set PATH "$RBENV_ROOT/shims" $PATH
+  end
+end
 
-      function rbenv
-        set command $argv[1]
-        set -e argv[1]
+function rbenv
+  if set -q __RBENV_SUPPORTS_FISH
+    command rbenv $argv
+  else
+    set command $argv[1]
+    set -e argv[1]
 
-        switch "$command"
-        case rehash shell
-          eval (rbenv "sh-$command" $argv)
-        case '*'
-          command rbenv "$command" $argv
-        end
-      end
+    switch "$command"
+    case rehash shell
+      eval (rbenv "sh-$command" $argv)
+    case '*'
+      command rbenv "$command" $argv
     end
   end
 end
+
