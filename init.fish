092a13c22390157c726fd77408e32ecba11b59c5
Justin Hileman <justin@justinhileman.info>
2016-03-06 13:06:02 -0800

    Initialize immediately, rather than waiting for an event.

--- init.fish
+++ init.fish
@@ -1,15 +1,11 @@
 # SYNOPSIS
 #   rbenv plugin for oh my fish
 
-function init --on-event init_rbenv
-  if not type -f -q rbenv; and set -q RBENV_ROOT; and not contains "$RBENV_ROOT/bin" $PATH
-    set PATH $RBENV_ROOT/bin $PATH
-  end
+if not type -fq rbenv; and set -q RBENV_ROOT; and not contains "$RBENV_ROOT/bin" $PATH
+  set PATH $RBENV_ROOT/bin $PATH
+end
 
-  if not type -q rbenv
-    echo "Please install 'rbenv' first, or set \$RBENV_ROOT!"; return 1
-  end
-
+if type -q rbenv
   if command rbenv init - | grep --quiet "function"
     set -gx __RBENV_SUPPORTS_FISH true
     source (rbenv init - | psub)
@@ -20,4 +16,6 @@ function init --on-event init_rbenv
 
     set PATH "$RBENV_ROOT/shims" $PATH
   end
+else
+  echo "Please install 'rbenv' first, or set \$RBENV_ROOT!"
 end
