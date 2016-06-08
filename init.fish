oh-my-fish/plugin-android-sdk

Only add missing paths during init.
This prevents issues when reloading the shell. See oh-my-fish/plugin-composer#7

init.fish
@@ -1,3 +1,9 @@
 set -q ANDROID_SDK_ROOT; or set -l ANDROID_SDK_ROOT /opt/android-sdk
 
-set PATH $ANDROID_SDK_ROOT/platform-tools $ANDROID_SDK_ROOT/tools $PATH
+if not contains $ANDROID_SDK_ROOT/tools $PATH
+  set PATH $ANDROID_SDK_ROOT/tools $PATH
+end
+
+if not contains $ANDROID_SDK_ROOT/platform-tools $PATH
+  set PATH $ANDROID_SDK_ROOT/platform-tools $PATH
+end

oh-my-fish/plugin-rbenv

init.fish
@@ -14,7 +14,9 @@ if type -q rbenv
       set -x RBENV_ROOT "$HOME/.rbenv"
     end
 
-    set PATH "$RBENV_ROOT/shims" $PATH
+    if not contains $RBENV_ROOT/shims $PATH
+      set PATH $RBENV_ROOT/shims $PATH
+    end
   end
 else
   echo "Please install 'rbenv' first, or set \$RBENV_ROOT!"